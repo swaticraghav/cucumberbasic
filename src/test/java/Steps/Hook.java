a40e4a5b2d449cf6f5af2e23657c5b4cbfc02c47
Swati Raghav <38935203+swaticraghav@users.noreply.github.com>
2019-05-16 14:34:36 +0530

    Update Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -1,13 +1,14 @@
-package Steps;
+package steps;
 
 import Base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 /**
- * Created by Karthik on 10/17/2016.
+ * Created by Karthik on 31/01/2019.
  */
+
 public class Hook extends BaseUtil{
 
 
@@ -18,19 +19,28 @@ public class Hook extends BaseUtil{
     }
 
     @Before
-    public void InitializeTest() {
+    public void InitializeTest(Scenario scenario) {
 
-        System.out.println("Opening the browser : MOCK");
 
-        //Passing a dummy WebDriver instance step info
-        base.StepInfo = "FirefoxDriver";
+        scenarioDef = base.features.createNode(scenario.getName());
+
+        System.out.println("Opening the browser : Firefox");
+
+        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
+        base.Driver = new FirefoxDriver();*/
+
+
+        //Chrome driver
+        System.setProperty("webdriver.chrome.driver", "C:\Libs\chromedriver");
+        base.Driver = new ChromeDriver();
     }
 
 
     @After
     public void TearDownTest(Scenario scenario) {
         if (scenario.isFailed()) {
-         // removed the line here coz of one error while building the project.
+            //Take screenshot logic goes here
+            System.out.println(scenario.getName());
         }
         System.out.println("Closing the browser : MOCK");
     }
